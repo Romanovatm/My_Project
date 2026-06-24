9e88ffd2a031bf7d2aa8588ba7ea9076ea3933f1
Romanovatm <m10021994r@gmail.com>
2026-06-25 02:03:05 +0300

    add logger for funcs

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("logs/utils.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def transactions_info(path_to_json_file: str) -> list[dict]:
@@ -6,14 +14,20 @@ def transactions_info(path_to_json_file: str) -> list[dict]:
     Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными
     о финансовых транзакциях.
     """
+    logger.debug("Начало работы функции")
     try:
         with open(path_to_json_file, "r", encoding="UTF-8") as file:
             transactions = json.load(file)  # список словарей для работы в питоне
+            logger.debug("Успешное открытие json файла, перевод в Python object")
         if not transactions:  # если пустой
+            logger.error("Файл пустой!")
             return []
         elif type(transactions) is not list:  # если не список
+            logger.error("Тип входных данных не является списком")
             return []
         else:
+            logger.info("Успешное  завершение функции")
             return transactions
     except FileNotFoundError:
+        logger.error("Внимание! FileNotFoundError")
         return []
